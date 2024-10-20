9b7bb1b937f5b47bd7ea46f8c7de0154fe8edba2
Keeperorowner <qingfong0731@outlook.com>
2024-10-20 18:42:35 +0800

    Update main.rs

--- zygiskd/src/main.rs
+++ zygiskd/src/main.rs
@@ -11,8 +11,8 @@ use crate::constants::ZKSU_VERSION;
 fn init_android_logger(tag: &str) {
     android_logger::init_once(
         android_logger::Config::default()
-            .with_max_level(constants::MAX_LOG_LEVEL)
-            .with_tag(tag),
+            。with_max_level(constants::MAX_LOG_LEVEL)
+            。with_tag(tag),
     );
 }
 
@@ -30,18 +30,18 @@ fn start() {
         println!("root impl: {:?}", root_impl::get_impl());
         return;
     } else if args.len() == 2 && args[1] == "eable" {
-        let path = "/data/adb/modules/admirepowered/noable";
+        let path = "/data/adb/modules/Zygisk_mod/noable";
         if Path::new(path).exists() {
             fs::remove_file(path).expect("Failed to remove noable file");
         }
         return;
     } else if args.len() == 2 && args[1] == "dable" {
         // Create the noable file with content "1"
-        let path = "/data/adb/modules/admirepowered/noable";
+        let path = "/data/adb/modules/Zygisk_mod/noable";
         fs::write(path, "1").expect("Failed to write noable file");
         return;
     }else if args.len() == 2 && args[1] == "status" {
-        let path = "/data/adb/modules/admirepowered/noable";
+        let path = "/data/adb/modules/Zygisk_mod/noable";
         if Path::new(path).exists() {
             println!("1");
         } else {
